dedb3afd74331dbcbf9c9166f17ae9e19269314a
QuinDiesel <0908427@hr.nl>
2017-01-18 12:04:52 +0100

    python file waarmee je locatie en keypress kan zien

--- test/ResolutionDisplay.py
+++ test/ResolutionDisplay.py
@@ -13,8 +13,8 @@ def process_events():
 
 class Game():
     def __init__(self):
-        width = 640
-        height = 480
+        width = 800
+        height = 600
         screen_size = (width, height) #gebonden resolutie
 
         pygame.init() #startgame
@@ -22,15 +22,14 @@ class Game():
         screen = pygame.display.set_mode(screen_size)#pygame code om scherm aan te roepen
         name = pygame.display.set_caption('Euromast')
         while not process_events():
-
-<<<<<<< HEAD
+          
             screen.fill((255,255,255)) #kleur display
             pygame.display.flip()
-=======
-    screen = pygame.display.set_mode(screen_size)  # pygame code om scherm aan te roepen
->>>>>>> aa1bf4841a576f06259fb2a3e320b487a7970eb5
 
-    def Buttons(self):
+            screen = pygame.display.set_mode(screen_size)  # pygame code om scherm aan te roepen
+
+
+
 
 
 def program():
