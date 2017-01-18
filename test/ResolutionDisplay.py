40248b043a392c2c548e4071b0dc6f860ab94788
QuinDiesel <0908427@hr.nl>
2017-01-18 11:28:55 +0100

    aangepast resolutie naar class

--- test/ResolutionDisplay.py
+++ test/ResolutionDisplay.py
@@ -11,21 +11,26 @@ def process_events():
 
     return False
 
+class Game():
+    def __init__(self):
+        width = 640
+        height = 480
+        screen_size = (width, height) #gebonden resolutie
+
+        pygame.init() #startgame
+
+        screen = pygame.display.set_mode(screen_size)#pygame code om scherm aan te roepen
+        name = pygame.display.set_caption('Euromast')
+        while not process_events():
+
+            screen.fill((255,255,255)) #kleur display
+            pygame.display.flip()
+
+    def Buttons(self):
+
 
 def program():
-    width = 640
-    height = 480
-    screen_size = (width, height) #gebonden resolutie
-
-    pygame.init() #startgame
-
-    screen = pygame.display.set_mode(screen_size)#pygame code om scherm aan te roepen
-
-    while not process_events():
-
-        screen.fill((255,255,255)) #kleur display
-        pygame.display.flip()
-
+    game = Game()
 
 
 program()
