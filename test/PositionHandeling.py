531c85a2671334412c893b812bfde3acfbb55177
QuinDiesel <0908427@hr.nl>
2017-01-18 12:40:35 +0100

    tot zover bezig

--- test/PositionHandeling.py
+++ test/PositionHandeling.py
@@ -2,17 +2,29 @@ import pygame
 
 pygame.init()
 
-gameDisplay = pygame.display.set_mode((800,600))
-pygame.display.set_caption("TESTLOCATIE")
+def color():
+    white = (255,255,255)
+    black = (0,0,0)
+    red = (255,0,0)
+    green = (255,0,0)
+    blue = (0,0,255)
+    yellow = (255,255,0)
 
-pygame.display.update()
+def Game():
+    gameDisplay = pygame.display.set_mode((800,600))
+    pygame.display.set_caption("TESTLOCATIE")
 
-gameExit = False
+    pygame.display.update()
 
-while not gameExit:
-    for event in pygame.event.get():
-        print(event)
+    gameExit = False
 
-pygame.quit()
+    while not gameExit: #while loop om kruisje te gebruiken
+        for event in pygame.event.get():
+             #print(event) #haal hashtag weg om position te zien
+            if event.type == pygame.QUIT:
+                gameExit = True
 
-quit()
+    pygame.quit()
+    quit()
+
+Game()
