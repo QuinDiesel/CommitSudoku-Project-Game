d011807597d983e044dffe113cc038f398ef6262
JurrianK <jurriankole1997@gmail.com>
2017-01-19 15:28:38 +0100

    Game kan worden aangeroepen door *.startGame(Game) aan te roepen

--- test/Game.py
+++ test/Game.py
@@ -1,70 +1,70 @@
 import pygame, sys
 from pygame.locals import *
 
-# Setup voor de verschillende categorieën
-sports = 0
-geography = 1
-entertainment = 2
-history = 3
-start = 4
 
-# Alle tegels tekenen en toevoegen aan een tilemap
-tilemap = [
-    [entertainment, history, sports, geography],
-    [entertainment, history, sports, geography],
-    [entertainment, history, sports, geography],
-    [entertainment, history, sports, geography],
-    [entertainment, history, sports, geography],
-    [entertainment, history, sports, geography],
-    [entertainment, history, sports, geography],
-    [entertainment, history, sports, geography],
-    [entertainment, history, sports, geography],
-    [entertainment, history, sports, geography],
-    [entertainment, history, sports, geography],
-    [entertainment, history, sports, geography],
-    [entertainment, history, sports, geography],
-    [entertainment, history, sports, geography],
-    [entertainment, history, sports, geography],
-    [start, start, start, start]
-]
+class Game:
+    def startGame(self):
+        # Setup voor de verschillende categorieën
+        sports = 0
+        geography = 1
+        entertainment = 2
+        history = 3
+        start = 4
 
-# Setup voor de kleuren
-blue = (0, 0, 255)
-green = (0, 255, 0)
-red = (255, 0, 0)
-yellow = (255, 255, 0)
-black = (0, 0, 0)
+        # Setup voor de afbeeldingen per categorie
+        textures = {
+            entertainment: pygame.image.load("img/Rood Vakje.jpg"),
+            history: pygame.image.load("img/Geel Vakje.jpg"),
+            sports: pygame.image.load("img/Blauw Vakje.jpg"),
+            geography: pygame.image.load("img/Groen Vakje.jpg"),
+            start: pygame.image.load("img/QuitButton.png")
+        }
 
-# Link de kleuren aan de 4 categorieën
-colours = {
-    entertainment: red,
-    history: yellow,
-    sports: blue,
-    geography: green,
-    start: black
-}
+        # Alle tegels tekenen en toevoegen aan een tilemap
+        tilemap = [
+            [entertainment, history, sports, geography],
+            [entertainment, history, sports, geography],
+            [entertainment, history, sports, geography],
+            [entertainment, history, sports, geography],
+            [entertainment, history, sports, geography],
+            [entertainment, history, sports, geography],
+            [entertainment, history, sports, geography],
+            [entertainment, history, sports, geography],
+            [entertainment, history, sports, geography],
+            [entertainment, history, sports, geography],
+            [entertainment, history, sports, geography],
+            [entertainment, history, sports, geography],
+            [entertainment, history, sports, geography],
+            [entertainment, history, sports, geography],
+            [entertainment, history, sports, geography],
+            [start, start, start, start]
+        ]
 
-# Dimensies van de map
-tilesize = 200
-mapwidth = 4
-mapheight = 16
+        # Dimensies van de map
+        tilesize = 40
+        mapwidth = 4
+        mapheight = 16
 
-# Test display plaatsen
-pygame.init()
-surface = pygame.display.set_mode((800, 600))
+        # Test display plaatsen
+        pygame.init()
+        surface = pygame.display.set_mode((800, 600))
 
-while True:
-    # Pak alle user events (quit e.d.)
-    for event in pygame.event.get():
-        # Check of de user wil stoppen
-        if event.type == QUIT:
-            pygame.quit()
-            sys.exit()
+        while True:
+            # Pak alle user events (quit e.d.)
+            for event in pygame.event.get():
+                # Check of de user wil stoppen
+                if event.type == QUIT:
+                    pygame.quit()
+                    sys.exit()
 
-    # Loop door alle reien om de map te tekenen
-    for row in range(mapheight):
-        for column in range(mapwidth):
-            # Teken een vierkantje met kleur
-            pygame.draw.rect(surface, colours[tilemap[row][column]], (column * tilesize, row * 38, tilesize, tilesize))
+            # Loop door alle reien om de map te tekenen
+            for row in range(mapheight):
+                for column in range(mapwidth):
+                    # Teken een vierkantje met kleur
+                    surface.blit(textures[tilemap[row][column]], (column * 200, row * tilesize))
 
-    pygame.display.update()
+            pygame.display.update()
+
+
+a = Game
+a.startGame(Game)
