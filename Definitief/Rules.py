ad256d8cf479608b46a1f5b6c9c583f95d144f7e
JurrianK <jurriankole1997@gmail.com>
2017-01-23 11:38:51 +0100

    Definitief folder opgebouwd

---
+++ Definitief/Rules.py
@@ -0,0 +1,28 @@
+import pygame
+import Definitief.Menu
+
+
+def startRules():
+    gameDisplay = pygame.display.set_mode((800, 600))
+    pygame.display.set_caption("Rules")
+
+    bg = pygame.image.load('img/RulesView.jpg')
+
+    loop = True
+    phase = "rules"
+
+    while loop:
+        # Check if user wants to quit
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+        gameDisplay.fill((0, 0, 0))
+
+        if phase == "rules":
+            gameDisplay.blit(bg, (0, 0))
+            pygame.display.flip()
+
+            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                Definitief.Menu.start_program()
